e9579ba53bd87f08aa6bc76fb83a114a835a078c
DESKTOP-2D9MENH\woong <woongsu0614@gmail.com>
2021-01-29 04:40:45 +0900

    strange change

--- server/app.py
+++ server/app.py
@@ -1,64 +1,78 @@
-from flask import Flask, request, jsonify
+from flask import Flask, flash, request, redirect, url_for, session, jsonify
 from storage import insert_calculation, get_calculations
 from syndicai import PythonPredictor
+from werkzeug.utils import secure_filename
+import logging
+import datetime
 
+from flask_cors import CORS, cross_origin
+
+import os
+
+from upload import uploader
 
 app = Flask(__name__)
+CORS(app)
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger('HELLO WORLD')
 
 @app.route('/')
 def index():
     return "Helmet Detection", 200
 
-@app.route('/health')
-def health():
-    return '', 200
 
-@app.route('/ready')
-def ready():
-    return '', 200
-
-@app.route('/data', methods=['GET'])
-def data():
-    '''
-        Function used to get calculations history
-        from Postgres database and return to fetch call in frontend.
-    :return: Json format of either collected calculations or error message
-    '''
-
-    calculations_history = []
-
-    try:
-        calculations = get_calculations()
-        for key, value in calculations.items():
-            calculations_history.append(value)
     
-        return jsonify({'calculations': calculations_history}), 200
-    except:
-        return jsonify({'error': 'error fetching calculations history'}), 500
+@app.route('/images', methods=['POST'])
+def process():
+    fileUpload()
+    #predict()
+    #updatedb()
+ 
+UPLOAD_FOLDER = '/usr/src/app'
+ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+  
+def fileUpload():
+    target=os.path.join(UPLOAD_FOLDER,'imgfolder')
+    if not os.path.isdir(target):
+        os.mkdir(target)
+    logger.info("welcome to upload`")
 
-@app.route('/insert_nums', methods=['POST'])
-def insert_nums():
-    '''
-        Function used to insert a calculation into our postgres
-        DB. Operands of operation received from frontend.
-    :return: Json format of either success or failure response.
-    '''
+    #SAVE NAME : DATE
+    now = datetime.datetime.now() # 2015-04-19 12:11:32.669083
+    nowDate = now.strftime('%Y-%m-%d')# 2015-04-19
+    nowTime = now.strftime('%H:%M:%S')# 12:11:32
+    nowDatetime = now.strftime('%Y-%m-%d %H:%M:%S')# 2015-04-19 12:11:32
 
-    insert_nums = request.get_json()
-    firstNum, secondNum, answer = insert_nums['firstNum'], insert_nums['secondNum'], insert_nums['answer']
+    #attaching date to name
+    file = request.files['file']
+    print(file)
+    file.save(file)
 
-    try:
-        insert_calculation(firstNum, secondNum, answer)
-        return jsonify({'Response': 'Successfully inserted into DB'}), 200
-    except:
-        return jsonify({'Response': 'Unable to insert into DB'}), 500
+    filename = "".join([nowDatetime, secure_filename(file.filename)])
+    print(filename)
+    file.save(filename)
 
-@app.route('/predict')
+    destination="/".join([target, filename])
+    print(destination)
+    file.save(destination)
+    #session['uploadFilePath']=filename
+    session['uploadFilePath']=destination
+    response={'response': 'hello', 'fileurl': destination}
+    uploadfile()
+
+    #Json 형태로 
+    return jsonify(response)
+
 def predict():
     """ Return JSON serializable output from the model """
     payload = request.args
     detector = PythonPredictor("")
     return detector.predict(payload)
+ 
+def updatedb():
+    pass
 
-
-        
+def uploadfile():
+    pass
