60b89324d09e7e82e29e7169a00822e7ff9f08ef
김효진 <gywls00100@naver.com>
2021-01-31 16:55:32 +0900

    [MOD] main page 문구 수정, bold추가

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,10 @@
 import React, { Component } from "react";
 import Camera from "./Camera";
 import AppBar from "@material-ui/core/AppBar";
-import Button from "@material-ui/core/Button";
-import CameraIcon from "@material-ui/icons/PhotoCamera";
-import Card from "@material-ui/core/Card";
-import CardActions from "@material-ui/core/CardActions";
-import CardContent from "@material-ui/core/CardContent";
-import CardMedia from "@material-ui/core/CardMedia";
 import CssBaseline from "@material-ui/core/CssBaseline";
-import Grid from "@material-ui/core/Grid";
+
 import Toolbar from "@material-ui/core/Toolbar";
+import CameraIcon from "@material-ui/icons/PhotoCamera";
 import Typography from "@material-ui/core/Typography";
 import { makeStyles } from "@material-ui/core/styles";
 import Container from "@material-ui/core/Container";
@@ -35,24 +30,6 @@ const useStyles = makeStyles((theme) => ({
   heroContent: {
     backgroundColor: theme.palette.background.paper,
     padding: theme.spacing(8, 0, 6),
-  },
-  heroButtons: {
-    marginTop: theme.spacing(4),
-  },
-  cardGrid: {
-    paddingTop: theme.spacing(8),
-    paddingBottom: theme.spacing(8),
-  },
-  card: {
-    height: "100%",
-    display: "flex",
-    flexDirection: "column",
-  },
-  cardMedia: {
-    paddingTop: "56.25%", // 16:9
-  },
-  cardContent: {
-    flexGrow: 1,
   },
   footer: {
     backgroundColor: theme.palette.background.paper,
@@ -88,7 +65,7 @@ class App extends Component {
                   gutterBottom
                 >
                   <br></br>
-                  Helmet Detection
+                  <b>Helmet Detection</b>
                 </Typography>
                 <Typography
                   variant="h5"
@@ -96,9 +73,9 @@ class App extends Component {
                   color="textSecondary"
                   paragraph
                 >
-                  잠깐! 헬멧 착용하셨나요?<br></br>
-                  헬멧을 착용한 상태에서 캡쳐 버튼을 누른 후 카메라를 5초 간
-                  응시해주세요.
+                  <h2>잠깐!</h2> 헬멧 착용하셨나요?<br></br>
+                  헬멧을 착용한 상태에서 <b>캡쳐 버튼</b>을 누른 후<br></br>
+                  <b>카메라를 5초 간 응시</b>해주세요.
                 </Typography>
 
                 <Camera></Camera>
