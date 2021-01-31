2be534da3d50e9539cc6e295706d3dcfa84d42af
김효진 <gywls00100@naver.com>
2021-02-01 01:16:42 +0900

    [ADD] 'Home'화면에서 start 누르면 'Webcam'페이지로 이동

--- client/src/pages/Webcam.js
+++ client/src/pages/Webcam.js
@@ -1,10 +1,138 @@
-import React from 'react';
+import React, { Component } from "react";
+import Webcam from "react-webcam";
+import ReactJson from "react-json-view";
+import Grid from "@material-ui/core/Grid";
+import { makeStyles } from "@material-ui/core/styles";
+import Container from "@material-ui/core/Container";
+import Paper from "@material-ui/core/Paper";
+import Fab from "@material-ui/core/Fab";
 
-const Webcam = () => {
-  return (
-    <div>
-       Webcam
-    </div>
-  );
-};
+class Webcam extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      screenshot: null,
+      result: null,
+    };
+  }
+88
+  setRef = (webcam) => {
+    this.webcam = webcam;
+  };
+
+  capture = () => {
+    const imageSrc = this.webcam.getScreenshot();
+    this.setState({ screenshot: imageSrc });
+  };
+
+  uploadImage = (image) => {
+    const url = "http://localhost:8000/images";
+    const data = new FormData();
+
+    fetch(image)
+      .then((res) => res.blob())
+      .then((blob) => {
+        data.append("file", blob, "face.jpg");
+
+        const options = {
+          method: "post",
+          contentType: false,
+          body: data,
+        };
+
+        fetch(url, options)
+          .then((res) => res.json())
+          .then((res) => {
+            console.log(res);
+            this.setState({
+              result: res,
+            });
+          });
+      });
+  };
+
+  render() {
+    const { screenshot, result } = this.state;
+
+    const useStyles = makeStyles((theme) => ({
+      heroButtons: {
+        marginTop: theme.spacing(4),
+      },
+      root: {
+        flexGrow: 1,
+      },
+      paper: {
+        padding: theme.spacing(2),
+        textAlign: "center",
+        color: theme.palette.text.secondary,
+      },
+      margin: {
+        margin: theme.spacing(1),
+      },
+    }));
+
+    return (
+      <center>
+        <Container maxWidth="sm" className={useStyles.root}>
+          <Grid item md={12}>
+            <Paper className={useStyles.paper}>
+              <div>
+                <Webcam
+                  audio={false}
+                  height={400}
+                  width={400}
+                  ref={this.setRef}
+                  screenshotFormat="image/jpg"
+                />
+              </div>
+              <Fab
+                variant="extended"
+                color="primary"
+                aria-label="add"
+                className={useStyles.margin}
+                onClick={() => this.capture()}
+              >
+                Capture
+              </Fab>
+            </Paper>
+          </Grid>
+          <br></br>
+          <br></br>
+          <Grid item md="auto">
+            <Paper className={useStyles.paper}>
+              <div>
+                {screenshot && (
+                  <img
+                    padding={10}
+                    src={screenshot}
+                    alt="screenshot"
+                    height={400}
+                    width={400}
+                  />
+                )}
+              </div>
+              <Fab
+                variant="extended"
+                color="secondary"
+                aria-label="add"
+                className={useStyles.margin}
+                onClick={() => this.uploadImage()}
+              >
+                Upload
+              </Fab>
+            </Paper>
+          </Grid>
+        </Container>
+
+        <div>
+          <div>
+            <h2>Result</h2>
+            {result && <ReactJson src={result} />}
+          </div>
+        </div>
+      </center>
+    );
+  }
+}
+
 export default Webcam;
--- client/src/shared/App.js
+++ client/src/shared/App.js
@@ -1,46 +1,10 @@
 import React, { Component } from "react";
 import { Route } from "react-router-dom";
-import Camera from "../pages/Camera";
-import AppBar from "@material-ui/core/AppBar";
-import CssBaseline from "@material-ui/core/CssBaseline";
-import Toolbar from "@material-ui/core/Toolbar";
-import CameraIcon from "@material-ui/icons/PhotoCamera";
-import Typography from "@material-ui/core/Typography";
-import { makeStyles } from "@material-ui/core/styles";
-import Container from "@material-ui/core/Container";
-import Link from "@material-ui/core/Link";
-
 import Home from "../pages/Home";
 import Webcam from "../pages/Webcam";
 import Result from "../pages/Result";
 import Success from "../pages/Success";
 
-function Copyright() {
-  return (
-    <Typography variant="body2" color="textSecondary" align="center">
-      {"Copyright © "}
-      <Link color="inherit" href="https://material-ui.com/">
-        GideokPark, DongyoungKim, WoongsuKim, HyojinKim
-      </Link>{" "}
-      {new Date().getFullYear()}
-      {"."}
-    </Typography>
-  );
-}
-
-const useStyles = makeStyles((theme) => ({
-  icon: {
-    marginRight: theme.spacing(2),
-  },
-  heroContent: {
-    backgroundColor: theme.palette.background.paper,
-    padding: theme.spacing(8, 0, 6),
-  },
-  footer: {
-    backgroundColor: theme.palette.background.paper,
-    padding: theme.spacing(6),
-  },
-}));
 
 class App extends Component {
   render() {
@@ -52,65 +16,6 @@ class App extends Component {
           <Route path="/result" component={Result} />
           <Route path="/success" component={Success} />
         </div>
-        <React.Fragment>
-          <CssBaseline />
-          <AppBar position="relative">
-            <Toolbar>
-              <CameraIcon className={useStyles.icon} />
-              <Typography variant="h6" color="inherit" noWrap>
-                MSBT : Helmet Detection
-                <br></br>
-              </Typography>
-            </Toolbar>
-          </AppBar>
-
-          <main>
-            {/* Hero unit */}
-            <div className={useStyles.heroContent}>
-              <Container maxWidth="sm">
-                <Typography
-                  component="h1"
-                  variant="h2"
-                  align="center"
-                  color="textPrimary"
-                  gutterBottom
-                >
-                  <br></br>
-                  <b>Helmet Detection</b>
-                </Typography>
-                <Typography
-                  variant="h5"
-                  align="center"
-                  color="textSecondary"
-                  paragraph
-                >
-                  <h2>잠깐!</h2> 헬멧 착용하셨나요?<br></br>
-                  헬멧을 착용한 상태에서 <b></b>캡쳐 버튼을 누른 후<br></br>
-                  <b>카메라를 5초 간 응시</b>해주세요.
-                </Typography>
-
-                <Camera></Camera>
-              </Container>
-            </div>
-          </main>
-
-          {/* Footer */}
-          <footer className={useStyles.footer}>
-            <Typography variant="h6" align="center" gutterBottom>
-              Footer
-            </Typography>
-            <Typography
-              variant="subtitle1"
-              align="center"
-              color="textSecondary"
-              component="p"
-            >
-              Something here to give the footer a purpose!
-            </Typography>
-            <Copyright />
-          </footer>
-          {/* End footer */}
-        </React.Fragment>
       </div>
     );
   }
