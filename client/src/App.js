fc5144336c82826fe4ec3acf68414c1864b56fd0
김효진 <gywls00100@naver.com>
2021-01-31 14:01:55 +0900

    [MOD] card 제거

--- client/src/App.js
+++ client/src/App.js
@@ -60,7 +60,6 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-const cards = [1, 2, 3];
 class App extends Component {
   render() {
     return (
@@ -105,39 +104,6 @@ class App extends Component {
                 <Camera></Camera>
               </Container>
             </div>
-            <Container className={useStyles.cardGrid} maxWidth="md">
-              {/* End hero unit */}
-              <Grid container spacing={4}>
-                {cards.map((card) => (
-                  <Grid item key={card} xs={12} sm={6} md={4}>
-                    <Card className={useStyles.card}>
-                      <CardMedia
-                        className={useStyles.cardMedia}
-                        image="https://source.unsplash.com/random"
-                        title="Image title"
-                      />
-                      <CardContent className={useStyles.cardContent}>
-                        <Typography gutterBottom variant="h5" component="h2">
-                          Heading
-                        </Typography>
-                        <Typography>
-                          This is a media card. You can use this section to
-                          describe the content.
-                        </Typography>
-                      </CardContent>
-                      <CardActions>
-                        <Button size="small" color="primary">
-                          View
-                        </Button>
-                        <Button size="small" color="primary">
-                          Edit
-                        </Button>
-                      </CardActions>
-                    </Card>
-                  </Grid>
-                ))}
-              </Grid>
-            </Container>
           </main>
 
           {/* Footer */}
