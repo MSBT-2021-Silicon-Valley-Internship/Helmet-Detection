7daedb406019f741e29fb6a9750fd1c7ae178fed
김효진 <gywls00100@naver.com>
2021-02-01 10:54:08 +0900

    [ADD] img 누르면 알림창 나오는 modal 추가

--- client/src/pages/Result.js
+++ client/src/pages/Result.js
@@ -4,6 +4,63 @@ import Paper from "@material-ui/core/Paper";
 import Fab from "@material-ui/core/Fab";
 import Container from "@material-ui/core/Container";
 import { makeStyles } from "@material-ui/core/styles";
+import Button from "@material-ui/core/Button";
+import Dialog from "@material-ui/core/Dialog";
+import DialogActions from "@material-ui/core/DialogActions";
+import DialogContent from "@material-ui/core/DialogContent";
+import DialogContentText from "@material-ui/core/DialogContentText";
+import DialogTitle from "@material-ui/core/DialogTitle";
+
+export function AlertDialog() {
+  const [open, setOpen] = React.useState(false);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  return (
+    <div>
+      <img
+        src="https://aosa.org/wp-content/uploads/2019/04/image-placeholder-350x350.png"
+        height={400}
+        width={400}
+        alt="placeholder"
+        onClick={handleClickOpen}
+      ></img>
+
+      <Dialog
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="alert-dialog-title"
+        aria-describedby="alert-dialog-description"
+      >
+        <DialogTitle id="alert-dialog-title">
+          {"Use Google's location service?"}
+        </DialogTitle>
+        <DialogContent>
+          <DialogContentText id="alert-dialog-description">
+            Let Google help apps determine location. This means sending
+            anonymous location data to Google, even when no apps are running.
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose} color="primary">
+            Cancle
+          </Button>
+          <RouterLink to="/success">
+            <Button onClick={handleClose} color="primary" autoFocus>
+              OK
+            </Button>
+          </RouterLink>
+        </DialogActions>
+      </Dialog>
+    </div>
+  );
+}
 
 class Result extends Component {
   constructor(props) {
@@ -62,14 +119,7 @@ class Result extends Component {
         </Container>
         <Container>
           <Paper className={useStyles.paper}>
-            <div>
-              <img
-                src="https://aosa.org/wp-content/uploads/2019/04/image-placeholder-350x350.png"
-                height={400}
-                width={400}
-                alt="placeholder"
-              ></img>
-            </div>
+            <AlertDialog></AlertDialog>
           </Paper>
         </Container>
       </center>
