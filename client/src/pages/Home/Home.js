060899230598a9e77ada5efd5e6197c89a87c426
김효진 <gywls00100@naver.com>
2021-02-02 11:49:11 +0900

    [ADD] stepbox 추가

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -1,20 +1,124 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "./Home.css";
+import { makeStyles } from "@material-ui/core/styles";
+import Stepper from "@material-ui/core/Stepper";
+import Step from "@material-ui/core/Step";
+import StepLabel from "@material-ui/core/StepLabel";
+import Button from "@material-ui/core/Button";
+import Typography from "@material-ui/core/Typography";
+import { Container } from "@material-ui/core";
 
 const Home = () => {
   return (
-    <div id="start-page">
-      <img
-        src="https://img.icons8.com/ios/150/000000/bicycle-helmet.png"
-        alt="helmet"
-      />
-      <h1>Helmet Detection</h1>
-      <h2>Be aware of Safety!</h2>
-      <Link to="/camera">
-        <button>Start</button>
-      </Link>
-    </div>
+    <Container>
+      
+      <div id="start-page">
+        <img
+          src="https://img.icons8.com/ios/150/000000/bicycle-helmet.png"
+          alt="helmet"
+        />
+        <h1>Helmet Detection AI</h1>
+        <h2>Be aware of Safety!</h2>
+        <HorizontalLabelPositionBelowStepper/>
+
+        <Link to="/camera">
+          <button>Start</button>
+        </Link>
+      </div>
+    </Container>
   );
 };
 export default Home;
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    width: "100%",
+  },
+  backButton: {
+    marginRight: theme.spacing(1),
+  },
+  instructions: {
+    marginTop: theme.spacing(1),
+    marginBottom: theme.spacing(1),
+  },
+}));
+
+function getSteps() {
+  return [
+    "Step1 : Capture",
+    "Step2 : Upload",
+    "Step3 : Check The Result",
+  ];
+}
+
+function getStepContent(stepIndex) {
+  switch (stepIndex) {
+    case 0:
+      return "Look at the camera with your helmet on.";
+    case 1:
+      return "Upload the captured image.";
+    case 2:
+      return "Check the results. We'll tell you if you're wearing a helmet or not.";
+    default:
+      return "Unknown stepIndex";
+  }
+}
+
+export function HorizontalLabelPositionBelowStepper() {
+  const classes = useStyles();
+  const [activeStep, setActiveStep] = React.useState(0);
+  const steps = getSteps();
+
+  const handleNext = () => {
+    setActiveStep((prevActiveStep) => prevActiveStep + 1);
+  };
+
+  const handleBack = () => {
+    setActiveStep((prevActiveStep) => prevActiveStep - 1);
+  };
+
+  const handleReset = () => {
+    setActiveStep(0);
+  };
+
+  return (
+    <div className={classes.root}>
+      <Stepper activeStep={activeStep} alternativeLabel>
+        {steps.map((label) => (
+          <Step key={label}>
+            <StepLabel>{label}</StepLabel>
+          </Step>
+        ))}
+      </Stepper>
+      <div>
+        {activeStep === steps.length ? (
+          <div>
+            <Typography className={classes.instructions}>
+              All steps completed
+            </Typography>
+            <Button onClick={handleReset}>Reset</Button>
+          </div>
+        ) : (
+          <div>
+            <Typography className={classes.instructions}>
+              {getStepContent(activeStep)}
+            </Typography>
+            <div>
+              <Button
+                disabled={activeStep === 0}
+                onClick={handleBack}
+                className={classes.backButton}
+              >
+                Back
+              </Button>
+              <Button variant="contained" color="primary" onClick={handleNext}>
+                {activeStep === steps.length - 1 ? "Finish" : "Next"}
+              </Button>
+            </div>
+          </div>
+        )}
+      </div>
+    </div>
+  );
+}
